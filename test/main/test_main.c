de1e654bbcc4e6b26f8559d40c45651937860522
Al West <al@tsew.net>
2025-07-08 00:40:46 +0100

    Update test_main.c

--- test/main/test_main.c
+++ test/main/test_main.c
@@ -1,26 +1,57 @@
+#include <stdint.h>
+#include <stdlib.h>
 #include "unity.h"
 #include "test_utils.h"
 #include "test_http_handlers.h"
 #include "test_nvs_utils.h"
 
-void app_main(void) {
+/**
+ * @brief Tells the QEMU emulator to exit with a success status code.
+ */
+static void qemu_exit_success(void)
+{
+    // The address of the QEMU power-off device
+    volatile uint32_t *addr = (volatile uint32_t *)0x3FF00004;
+    // Write the magic value to trigger a successful power-off
+    *addr = 1;
+}
+
+/**
+ * @brief Tells the QEMU emulator to exit with a failure status code.
+ */
+static void qemu_exit_failure(void)
+{
+    volatile uint32_t *addr = (volatile uint32_t *)0x3FF00004;
+    // Write a different value (e.g., 3) for failure. QEMU exits with status (val >> 1).
+    *addr = 3;
+}
+
+void app_main(void)
+{
     UNITY_BEGIN();
-    
+
     // Utility tests
     RUN_TEST(test_url_decode_basic);
     RUN_TEST(test_url_decode_plus_sign);
     RUN_TEST(test_url_decode_hex_chars);
     RUN_TEST(test_basic_string_validation);
-    
+
     // HTTP handler tests
     RUN_TEST(test_http_handler_mock);
     RUN_TEST(test_request_validation_mock);
-    
+
     // NVS tests
     RUN_TEST(test_nvs_mock);
     RUN_TEST(test_settings_persistence_mock);
-    
-    UNITY_END();
 
-    exit(0);
+    // UNITY_END() returns the number of failures.
+    int failures = UNITY_END();
+
+    if (failures > 0) {
+        // If any test failed, exit QEMU with a failure code
+        qemu_exit_failure();
+    } else {
+        // Otherwise, exit QEMU with a success code
+        qemu_exit_success();
+    }
 }
